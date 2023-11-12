d1ac309a7c7bc1cccc90b0f8f22ae71ec036b7ea
fuglyuckDev <harrisoncwalton@gmail.com>
2023-11-12 18:16:02 +0000

    A sign up form - front end mentor
    This is my first front-end mentor project, I thought I'd go back to the
    basics with HTML, CSS and JS.
    This took longer than expected as i'm used to Next.js
    I Need to get my work time down as this took me 4/5 hours

---
+++ logic.js
@@ -0,0 +1,30 @@
+const signUpElement = document.getElementById("sign__up__form__container");
+const input = document.getElementById("monthly__news__letter__input");
+const submitButton = document.getElementById(
+  "monthly__news__letter__subscribe__button"
+);
+const form = document.getElementById("monthly__news__letter__form");
+const error = document.getElementById("email__error__message");
+const success = document.getElementById("success__message__container");
+const dismissButton = document.getElementById(
+  "success__message__dismiss__button"
+);
+
+submitButton.addEventListener("click", (e) => {
+  if (input.value.includes("@")) {
+    e.preventDefault();
+    signUpElement.style.setProperty("display", "none");
+    success.style.setProperty("display", "flex");
+  } else {
+    e.preventDefault();
+    console.log("email does not have an @ symbol");
+    input.style.setProperty("border", "1px solid red");
+    input.style.setProperty("background-color", "#FFE8E6");
+    input.style.setProperty("color", "red");
+    error.style.setProperty("display", "block");
+  }
+});
+
+dismissButton.addEventListener("click", (e) => {
+  success.style.setProperty("display", "none");
+});
